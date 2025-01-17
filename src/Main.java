8ef1cb88fa2950b5f82175bba3505116b520a31b
Kseniya <eseniya.wi@gmail.com>
2025-01-17 18:03:02 +0300

    did lesson 3 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,108 @@
+import java.util.Scanner;
+
 public class Main {
+
+    /* Написать программу, которая
+    проверяет является ли число палиндромом */
+    public static void isPalindrome() {
+        System.out.println("Enter number: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        String numbersStr = "" + number;
+
+        // variant 1
+        String reverseNumber = new StringBuilder(numbersStr).reverse().toString();
+        boolean isPalindrome = numbersStr.equals(reverseNumber);
+
+        // variant 2
+        boolean isTrue = true;
+        char[] symbols = numbersStr.toCharArray();
+
+        for (int i=0; i< symbols.length; i++){
+            if (symbols[symbols.length - (i+1)] == symbols[i]){
+                isTrue = true;
+            } else {
+                isTrue = false;
+            }
+        }
+        // result
+        if (isPalindrome || isTrue){
+            System.out.println("Number is palindrome");
+        } else {
+            System.out.println("Number isn't palindrome");
+        }
+    }
+
+    /* Определить является ли число простым */
+    public static void isSimple() {
+        System.out.println("Enter number: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        boolean isSimple = true; // для проверки деления
+
+        for (int i = 2; i < number; i++) {
+            /* начинаем с 2, так как все числа делятся на 1
+            и строго < так как длина и есть само число */
+            if (number%i == 0 || number<=0){
+                /* проверяем есть ли остаток после деления либо на
+                отрицательность числа и 0, так как 0 не простое число*/
+                isSimple = false;
+                break;
+            }
+        }
+        // result
+        if (isSimple){
+            System.out.println("Number is simple");
+        } else {
+            System.out.println("Number isn't simple");
+        }
+    }
+
+    /* Вычислить сумму цифр числа до единичного разряда */
+    public static void calculateSum() {
+        System.out.println("Enter number: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        System.out.println("Sum of digits: " + reduceToSingleDigit(number));
+    }
+    private static int reduceToSingleDigit(int num) {
+        if (num < 10) {
+            return num;
+        }
+
+        return reduceToSingleDigit((num%10) + reduceToSingleDigit(num/10));
+    }
+
+    /* Вывеcти перевернутую лесенку высотой n */
+    public static void reverseStairs () {
+        System.out.println("Enter number of stairs: ");
+        Scanner scanner = new Scanner(System.in);
+        int numberOfStairs, count;
+
+        for (numberOfStairs=scanner.nextInt(); numberOfStairs > 0; numberOfStairs--) {
+            for (count = numberOfStairs; count > 0; count--)
+                System.out.print('#');
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Enter number of task: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+        switch (number) {
+            case 1: isPalindrome();
+                break;
+            case 2: isSimple();
+                break;
+            case 3: calculateSum();
+                break;
+            case 4: reverseStairs();
+                break;
+            default: System.out.println("Выход из программы");
+        }
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,82 @@
-class Main {
+/* Написать программу, которая
+ проверяет является ли число палиндромом */
+fun isPalindrome() {
+    println("Enter number: ")
+    val number = readln().toInt().toString()
+
+    when {
+        number == number.reversed() ->
+            println("Number is palindrome")
+        else ->
+            println("Number isn't palindrome")
+    }
+
+    return main()
+}
+
+/* Определить является ли число простым */
+fun isSimple() {
+    println("Enter number: ")
+    val number = readln().toInt()
+    var isTrue = true
+
+    for (count in 2 until number) {
+        if (number%count==0 || number<=0)
+            isTrue = false
+    }
+
+    when(isTrue) {
+        false -> println("Number isn't simple")
+        else -> println("Number is simple")
+    }
+
+    return main()
+}
+
+/* Вычислить сумму цифр числа до единичного разряда */
+fun sumOfDigits() {
+    println("Enter number: ")
+    var number = readln().toInt()
+
+    println(sumOfDigitsRecurse(number))
+
+    return main()
+}
+
+fun sumOfDigitsRecurse(num: Int): Int {
+    when(num) {
+        in 1..10 ->
+            return num
+        else -> {
+            return sumOfDigitsRecurse((num%10) + sumOfDigitsRecurse(num/10))
+        }
+    }
+}
+
+/* Вывеcти перевернутую лесенку высотой n */
+fun reverseStairs() {
+    println("Enter number: ")
+    var number = readln().toInt()
+
+    while (number > 0) {
+        for (count in 1 ..number) {
+            print('#')
+        }
+        println()
+        number--
+    }
+
+    return main()
+}
+
+fun main() {
+    println("\nEnter number of task: ")
+    val task = readln().toInt()
+    when(task) {
+        1 -> isPalindrome()
+        2 -> isSimple()
+        3 -> sumOfDigits()
+        4 -> reverseStairs()
+        else -> println("Exit...")
+    }
 }
