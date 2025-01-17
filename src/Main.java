f920e840328aa3fee2377d6ff21b8a3b75bbb8fa
Kseniya <eseniya.wi@gmail.com>
2025-01-17 18:00:35 +0300

    did lesson 2 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,178 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static public void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите номер задания: ");
+        int task = scanner.nextInt();
+        switch (task){
+            case 1:
+                tasksOneTwo();
+                break;
+            case 2:
+                tasksThreeFour();
+                break;
+            case 3:
+                taskFiveVariant1();
+                break;
+            case 4:
+                taskFiveVariant2();
+                break;
+            case 5:
+                findPerimeterAndArea();
+                break;
+            case 6:
+                calculateMark();
+                break;
+            case 7:
+                printMultiplicationTable();
+                break;
+            case 8:
+                fibonacci();
+                break;
+            default: System.out.println("Такого задания нет :(");
+        }
+    }
+
+    private static void tasksOneTwo() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите два целых числа: ");
+
+        int numOne = scanner.nextInt();
+        int numTwo = scanner.nextInt();
+
+        System.out.println("Сумма чисел: " + (numOne+numTwo));
+
+        if (numOne % 2 == 0 && numTwo % 2 == 0) {
+            System.out.println("Оба числа являются четными");
+        } else if (numOne % 2 == 0) {
+            System.out.println("Первое число является четным");
+        } else if(numTwo % 2 == 0) {
+            System.out.println("Второе число является четным");
+        } else {
+            System.out.println("Ни одно число не является четным");
+        }
+    }
+
+    private static void tasksThreeFour() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+
+        int num = scanner.nextInt();
+        int sum = 0;
+        int i = 1;
+
+        while (num > 0) {
+            System.out.println(i);
+            i+=1;
+            sum+=num;
+            num--;
+        }
+        System.out.println("Сумма чисел от 1 до " + (i-1) + " = " + sum);
+
+    }
+
+    private static void taskFiveVariant1() {
+        boolean isPositive = true;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите число: ");
+
+        while (isPositive){
+            int num = scanner.nextInt();
+            if (num >= 0) {
+                System.out.println("Число " + num);
+            } else {
+                isPositive = false;
+                System.out.println("Введено отрицательное число, программа завершена");
+            }
+        }
+    }
+
+    private static void taskFiveVariant2() {
+        int num = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+
+        do{
+            num = scanner.nextInt();
+            System.out.println("Число " + num);
+        }
+        while (num>=0);
+    }
+
+    private static void findPerimeterAndArea() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите длину и ширину прямоугольника: ");
+
+        int width = scanner.nextInt();
+        int height = scanner.nextInt();
+
+        System.out.println("Периметр прямоугольника: " + ((width+height) * 2));
+        System.out.println("Площадь прямоугольника: " + (width * height));
+    }
+
+    private static void calculateMark() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите баллы студента: ");
+
+        int mark = scanner.nextInt();
+
+        if (mark >=90){
+            System.out.println("Оценка А");
+        } else if (mark >= 75){
+            System.out.println("Оценка B");
+        } else if (mark >= 50){
+            System.out.println("Оценка С");
+        } else if (mark >= 30){
+            System.out.println("Оценка D");
+        } else{
+            System.out.println("Оценка F");
+        }
+    }
+
+    private static void printMultiplicationTable() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число: ");
+
+        int number = scanner.nextInt();
+        int secondNumber = 1;
+
+        while ((secondNumber) <= (number+1)){
+            System.out.println(number + " * " + secondNumber + " = " + number*secondNumber);
+            secondNumber++;
+        }
+    }
+
+    private static void fibonacci() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер числа последовательности: ");
+        int num = scanner.nextInt();
+        int result = fibonacciVariant2(num);
+        System.out.println(result);
+        fibonacciVariant1(num);
+    }
+
+    private static void fibonacciVariant1(int number) {
+        int counter = 2;
+        int firstNumber = 0;
+        int secondNumber = 1;
+        int num = 0;
+        while (counter<=number) {
+            num = firstNumber + secondNumber;
+            firstNumber = secondNumber;
+            secondNumber = num;
+            counter++;
+        }
+        System.out.println(num);
+    }
+
+    private static int fibonacciVariant2(int number) {
+        if (number<=1) {
+            return number;
+        }
+        return fibonacciVariant2(number-2) + fibonacciVariant2(number-1);
+    }
+
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,147 @@
-class Main {
+fun main() {
+    println( "Для выхода из программы введите отрицательное число\n"+
+            "Введите номер задания: ")
+    val num = readln().toInt()
+    when (num) {
+        in 1..2 -> tasksOneTwo()
+        in 3..4 -> taskThreeFour()
+        5 -> taskFive()
+        6 -> calculatePerimeterAndArea()
+        7 -> mark()
+        8 -> table()
+        9 -> fibonacci()
+        10 -> printString()
+        11 -> printNumbers ()
+        12 -> {
+            println("Введите что-либо")
+            val userInput = readln()
+            val anyInformation: Any = when {
+                userInput.toIntOrNull() != null -> userInput.toInt()
+                userInput.toDoubleOrNull() != null -> userInput.toDouble()
+                else -> userInput
+            }
+            anyPrint(anyInformation)
+        }
+        else -> println("Такого задания не существует")
+    }
+}
+
+fun tasksOneTwo() {
+    println("Введите два числа: ")
+    val numOne = readln().toInt()
+    val numTwo = readln().toInt()
+
+    println("Сумма двух чисел: ${numTwo+numOne}")
+    if (numOne % 2 == 0 && numTwo % 2 == 0) {
+        println("Оба числа четные")
+    } else if (numOne % 2 == 0){
+        println("Число один четное")
+    }
+    else if (numTwo % 2 == 0) {
+        println("Число два четное")
+    } else{
+        println("Ни одно число не четное")
+    }
+}
+
+fun taskThreeFour() {
+    println("Введите число: ")
+    var number = readln().toInt()
+    var i = 1;
+    var sum = 0
+    while (number < 0){
+        println("Число: $i")
+        sum+=number
+        i++
+        number--
+    }
+    println("Сумма чисел: $sum")
+}
+
+fun taskFive() {
+    println("Введите число: ")
+    var number: Int
+    do{
+        number = readln().toInt()
+        println("Число $number")
+    } while(number >= 0)
+}
+
+fun calculatePerimeterAndArea() {
+    val width = readln().toInt()
+    val height = readln().toInt()
+
+    println("Периметр прямоугольника: ${(width+height)*2}")
+    println("Площадь прямоугольника: ${width*height}")
+}
+
+fun mark() {
+    println("Введите баллы: ")
+    val points = readln().toInt()
+    when (points){
+        in 90..100 -> println("Оценка A")
+        in 75..89 -> println("Оценка B")
+        in 50..74 -> println("Оценка C")
+        in 30..49 -> println("Оценка D")
+        in 0..29 -> println("Оценка F")
+    }
+}
+
+fun table() {
+    println("Введите число: ")
+    var number = readln().toInt()
+    var secondNumber = 1
+    while (secondNumber<=number+1){
+        println("$number * $secondNumber = ${number*secondNumber}")
+        secondNumber++
+    }
+}
+
+fun fibonacci() {
+    println("Введите число: ")
+    var number = readln().toInt()
+    number = calculateFibonacci(number)
+    println(number)
+}
+
+fun calculateFibonacci (num: Int): Int {
+    if (num<=1){
+        return num
+    }
+    return (calculateFibonacci(num-1) + calculateFibonacci(num-2))
+}
+
+fun printString () {
+    var stroki: List<String?>
+    stroki = listOf("автомарафон", "чаеводство", "автоматчица", "галициийский", "благоприятствовавший" )
+    if (!stroki.isNullOrEmpty()){
+        stroki.forEach { element ->
+            val newElement = element.substring(0,10)
+            println(newElement)
+        }
+    }
+}
+
+fun printNumbers () {
+    for (num in 1..100){
+        when {
+            num % 3 == 0 && num % 5 == 0 -> println("FizzBuzz")
+            num % 3 == 0 -> println("Fizz")
+            num % 5 == 0 -> println("Buzz")
+            else -> println(num)
+        }
+    }
+}
+
+fun anyPrint (any: Any) {
+    when {
+        (any is String) && (any.length > 5 ) -> println("Это строка")
+        (any is Int) && (any >= 0) -> println("Число положительное")
+        (any is Int) && (any < 0) -> println("Число отрицательное")
+        (any is Double) && (any >= 0) -> println("Число положительное")
+        (any is Double) && (any < 0) -> println("Число отрицательное")
+        (any is Float) && (any >= 0) -> println("Число положительное")
+        (any is Float) && (any < 0) -> println("Число отрицательное")
+        else -> println("Неизвестный тип")
+    }
 }
