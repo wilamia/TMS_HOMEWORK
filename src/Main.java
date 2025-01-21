fd44dbbf68e17ef4b81ef170b529e314f82833e1
Kseniya <eseniya.wi@gmail.com>
2025-01-21 22:23:54 +0300

    did lesson4 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,69 @@
+import java.util.Scanner;
+
 public class Main {
+
+     /*Напишите программу, которая принимает строку и переставляет слова в обратном
+    порядке, используя только методы String и StringBuilder.*/
+
+    public static void reverseWords() {
+
+        Scanner scn = new Scanner(System.in);
+        StringBuilder currentWord = new StringBuilder();
+        StringBuilder reversedWord = new StringBuilder();
+
+        String message = scn.nextLine();
+
+        while (true) {
+            if (message.contains(" ")) {
+                int index = message.indexOf(" ");
+                currentWord.append(message, 0, index);
+                reversedWord.insert(0, currentWord + " ");
+                message = message.substring(index + 1).trim();
+                currentWord.setLength(0);
+            } else {
+                reversedWord.insert(0, message + " ").reverse();
+                break;
+            }
+        }
+        System.out.println(reversedWord);
+    }
+
+    /*Реализуйте программу, которая преобразует строку в последовательность ASCII￾
+    кодов и строку кодов в текст, используя циклы и методы String.*/
+
+    public static void shiftAscii() {
+        Scanner scn = new Scanner(System.in);
+        String message = scn.nextLine();
+        StringBuilder asciiMessage = new StringBuilder();
+        String[] asciiCodes = message.split(" ");
+        StringBuilder decodedMessage = new StringBuilder();
+        char[] symbols = message.toCharArray();
+        if (isAsciiCodes(message)) {
+            for (String code : asciiCodes) {
+                int asciiCode = Integer.parseInt(code);
+                decodedMessage.append((char) asciiCode);
+            }
+            System.out.println(decodedMessage);
+        } else {
+            for (char symbol : symbols) {
+                asciiMessage.append((int) symbol + " ");
+            }
+            System.out.println(asciiMessage);
+        }
+    }
+
+    public static boolean isAsciiCodes(String input) {
+        String[] parts = input.split(" ");
+        for (String part : parts) {
+            if (!part.matches("\\d+")) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        reverseWords();
+        shiftAscii();
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,35 @@
-class Main {
+/*Напишите программу, которая принимает строку и переставляет слова в обратном
+порядке, используя только методы String и StringBuilder.*/
+
+fun reverseWord() {
+    val message = readln()
+    val reversedWords = message.split(" ").reversed().joinToString(" ")
+    println(reversedWords.reversed())
+}
+
+/*Реализуйте программу, которая преобразует строку в последовательность ASCII￾
+кодов и строку кодов в текст, используя циклы и методы String.*/
+
+fun asciiCode() {
+    val message = readln()
+    if (message.all { it.isDigit() || it.isWhitespace() }) {
+        val asciiCodes = message.split(" ")
+        val decodedMessage = StringBuilder()
+
+        for (code in asciiCodes) {
+            val asciiCode = code.toInt()
+            decodedMessage.append(asciiCode.toChar())
+        }
+        println(decodedMessage)
+    } else {
+        for (char in message) {
+            print("${char.toInt()} ")
+        }
+    }
+
+}
+
+fun main() {
+    reverseWord()
+    asciiCode()
 }
