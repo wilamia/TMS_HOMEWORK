93df069871a0e855984b4350526bb4938005e545
Kseniya <eseniya.wi@gmail.com>
2025-01-24 14:58:11 +0300

    did lesson5 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,150 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        searchMaxMinValue();
+        countEvenOddNumbers();
+        reverseArray();
+        isPalindrome();
+
+    }
+
+    /*Напишите программу, которая находит и выводит максимальное и минимальное
+    значение в массиве целых чисел. Также вычислите индекс этих элементов.
+    */
+
+    public static void searchMaxMinValue() {
+        Random random = new Random();
+        int maxValue = Integer.MIN_VALUE;
+        int maxValueIndex = 0;
+        int minValue = Integer.MAX_VALUE;
+        int minValueIndex = 0;
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(-5, 5);
+        }
+
+        System.out.println("Массив чисел: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+            if (maxValue < array[i]) {
+                maxValue = array[i];
+                maxValueIndex = i;
+            } else if (minValue > array[i]) {
+                minValue = array[i];
+                minValueIndex = i;
+            }
+        }
+
+        System.out.println();
+        if (maxValue == 0 && minValue == 0) {
+            return;
+        } else {
+            System.out.println("Минимальное значение: " + minValue + "\nИндекс элемента: " + minValueIndex);
+            System.out.println("Максимальное значение: " + maxValue + "\nИндекс элемента: " + maxValueIndex);
+        }
+    }
+
+    /*Напишите программу, которая принимает массив целых чисел и подсчитывает,
+сколько четных и сколько нечетных чисел в нем содержится*/
+
+    public static void countEvenOddNumbers() {
+        Random random = new Random();
+        int evenNumbers = 0;
+        int oddNumbers = 0;
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(0, 15);
+        }
+
+        System.out.println("Массив чисел: ");
+        for (int value: array) {
+            System.out.print(value + " ");
+
+            if (value%2==0) {
+                evenNumbers+=1;
+            } else {
+                oddNumbers+=1;
+            }
+        }
+
+        System.out.println();
+        if(oddNumbers == 0 && evenNumbers == 0){
+            return;
+        }
+        else {
+            System.out.println("Количество четных чисел: " + evenNumbers +
+                    "\nКоличество нечетных чисел: " + oddNumbers);
+        }
+    }
+
+    /*Напишите программу, которая принимает массив целых чисел и изменяет его порядок
+на обратный. Выведите результат после изменения порядка.*/
+
+    public static void reverseArray() {
+        Random random = new Random();
+        int valueFirstHalf = 0;
+        int valueSecondHalf = 0;
+        int[] array = new int[11];
+        int counter = array.length - 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(0, 20);
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println("\nПеревернутый массив: ");
+        for (int i = 0; i < array.length / 2; i++) {
+            valueFirstHalf = array[i];
+            valueSecondHalf = array[counter];
+            array[i] = valueSecondHalf;
+            array[counter] = valueFirstHalf;
+            counter--;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    /*Напишите программу, которая проверяет,
+является ли массив чисел палиндромом*/
+
+    public static void isPalindrome() {
+        Scanner scn = new Scanner(System.in);
+        int valueFirstHalf = 0;
+        int valueSecondHalf = 0;
+        int[] array = new int[6];
+        int counter = array.length - 1;
+        boolean isPalindrome = true;
+
+        System.out.println("Введите " + array.length + " чисел массива: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scn.nextInt();
+        }
+
+        for (int value: array) {
+            System.out.print(value + " ");
+        }
+
+        System.out.println();
+        for (int i = 0; i < array.length / 2; i++) {
+            valueFirstHalf = array[i];
+            valueSecondHalf = array[counter];
+            if(valueFirstHalf != valueSecondHalf) {
+                isPalindrome = false;
+                break;
+            }
+            counter--;
+        }
+
+        if(isPalindrome) {
+            System.out.println("Массив является палиндромом");
+        } else {
+            System.out.println("Массив не является палиндромом");
+        }
+    }
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,151 @@
-class Main {
+import kotlin.random.Random
+
+fun main() {
+    searchMaxMinValue()
+    countEvenOddNumbers()
+    reverseArray()
+    isPalindrome()
+}
+
+/*Напишите программу, которая находит и выводит максимальное и минимальное
+значение в массиве целых чисел. Также вычислите индекс этих элементов*/
+
+fun searchMaxMinValue() {
+    var maxValue = Integer.MIN_VALUE;
+    var maxIndex = 0;
+    var minValue = Integer.MAX_VALUE;
+    var minIndex = 0;
+    var array = emptyArray<Int>()
+
+    println("Введите длину массива: ")
+    val length = readln().toInt()
+
+    for (i in 0 until length) {
+        array += Random.nextInt(1, 10)
+    }
+
+    for (i in 0 until length) {
+        print("${array[i]} ")
+        when {
+            array[i] > maxValue -> {
+                maxValue = array[i]
+                maxIndex = i;
+            }
+
+            array[i] < minValue -> {
+                minValue = array[i]
+                minIndex = i;
+            }
+        }
+    }
+
+    println()
+
+    when {
+        maxValue == 0 && minValue == 0 -> return
+        else -> println(
+            "Максимальное число: $maxValue" +
+                    "\nИндекс максимального числа: $maxIndex" +
+                    "\nМинимальное число: $minValue" +
+                    "\nИндекс минимального числа: $minIndex"
+        )
+    }
+}
+
+/*Напишите программу, которая принимает массив целых чисел и подсчитывает,
+сколько четных и сколько нечетных чисел в нем содержится*/
+
+fun countEvenOddNumbers() {
+    var evenNumbers = 0;
+    var oddNumbers = 0;
+    var array = emptyArray<Int>()
+
+    println("Введите длину массива: ")
+    val length = readln().toInt()
+    for (i in 0 until length) {
+        array += Random.nextInt(1, 10)
+        print("${array[i]} ")
+    }
+    println()
+
+    for (value in array) {
+        when {
+            value % 2 == 0 -> evenNumbers++
+            else -> oddNumbers++
+        }
+    }
+    when {
+        evenNumbers == 0 && oddNumbers == 0 -> return
+        else -> println("Количество четных чисел: $evenNumbers \nКоличество нечетных чисел: $oddNumbers")
+    }
+}
+
+/*Напишите программу, которая принимает массив целых чисел и изменяет его порядок
+на обратный. Выведите результат после изменения порядка*/
+
+fun reverseArray() {
+    var array = emptyArray<Int>()
+    var firstHalfValue: Int
+    var secondHalfValue: Int
+
+    println("Введите длину массива: ")
+    var length = readln().toInt()
+
+    println("Введите числа массива: ")
+    for (i in 0 until length) {
+        array += readln().toInt()
+    }
+
+    for (value in array) {
+        print("$value ")
+    }
+    println()
+
+    for (i in 0 until length / 2) {
+        firstHalfValue = array[i]
+        secondHalfValue = array[length - 1]
+        array[i] = secondHalfValue
+        array[length - 1] = firstHalfValue
+        length--
+    }
+
+    for (value in array) {
+        print("$value ")
+    }
+    println()
+}
+
+/*Напишите программу, которая проверяет,
+является ли массив чисел палиндромом.*/
+
+fun isPalindrome() {
+    var array = emptyArray<Int>()
+
+    println("Введите длину массива: ")
+    var length = readln().toInt()
+    var isPalindrome = true
+
+    println("Введите числа массива: ")
+    for (i in 0 until length) {
+        array += readln().toInt()
+    }
+
+    for (value in array) {
+        print("$value ")
+    }
+    println()
+
+    for (i in 0 until length / 2) {
+        if (array[i] != array[length - 1]) {
+            isPalindrome = false
+            break
+        }
+        length--
+    }
+
+    when {
+        isPalindrome -> println("Массив является палиндромом")
+        else -> println("Массив не является палиндромом")
+    }
+
 }
